a8ed99579a5d39d3386c903123054dcdc0ae6e25
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-04-21 08:34:58 +0700

    Update StaffDTO.java

--- DoAnBanDoChoiTreEm/src/main/java/Inventory/DTO/StaffDTO.java
+++ DoAnBanDoChoiTreEm/src/main/java/Inventory/DTO/StaffDTO.java
@@ -1,17 +1,54 @@
 package Inventory.DTO;
 
-
-public class StaffDTO extends CustomerDTO{
-    private int staStatus;
-    private String staRole;
+public class StaffDTO {
+    private int staStatus, staID;
+    private String staRole, staName, staEmail, staPhone;
     
     public StaffDTO(){}
 
-    public StaffDTO(int staStatus, String staRole) {
+    public StaffDTO(int staStatus, int staID, String staRole, String staName, String staEmail, String staPhone) {
         this.staStatus = staStatus;
+        this.staID = staID;
         this.staRole = staRole;
+        this.staName = staName;
+        this.staEmail = staEmail;
+        this.staPhone = staPhone;
     }
 
+    public int getStaID() {
+        return staID;
+    }
+
+    public void setStaID(int staID) {
+        this.staID = staID;
+    }
+
+    public String getStaName() {
+        return staName;
+    }
+
+    public void setStaName(String staName) {
+        this.staName = staName;
+    }
+
+    public String getStaEmail() {
+        return staEmail;
+    }
+
+    public void setStaEmail(String staEmail) {
+        this.staEmail = staEmail;
+    }
+
+    public String getStaPhone() {
+        return staPhone;
+    }
+
+    public void setStaPhone(String staPhone) {
+        this.staPhone = staPhone;
+    }
+
+    
+  
     public int getStaStatus() {
         return staStatus;
     }
