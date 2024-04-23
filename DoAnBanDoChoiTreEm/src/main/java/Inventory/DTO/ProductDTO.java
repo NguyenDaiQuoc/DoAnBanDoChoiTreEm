1338e10e644e87ff210e2254320d60a71dc629c2
Sang0912 <152009046+Sang0912@users.noreply.github.com>
2024-04-23 22:29:48 +0700

    Add files via upload

--- DoAnBanDoChoiTreEm/src/main/java/Inventory/DTO/ProductDTO.java
+++ DoAnBanDoChoiTreEm/src/main/java/Inventory/DTO/ProductDTO.java
@@ -1,69 +1,84 @@
-package Inventory.DTO;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DoAn;
 
-public class ProductDTO{
-    private String proID, proName, proOrigin;
-    private float proPrice;
-    private int proRemain, proQuantity;
+/**
+ *
+ * @author ADMIN
+ */
+public class ProductDTO {
+    private String id;
+    private String ten;
+    private String xuatxu;
+    private double gia;
+    private int soLuongConLai;
+    private int tongSanPham;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(String id, String ten, String xuatxu, double gia, int soLuongConLai, int tongSanPham) {
+        this.id = id;
+        this.ten = ten;
+        this.xuatxu = xuatxu;
+        this.gia = gia;
+        this.soLuongConLai = soLuongConLai;
+        this.tongSanPham = tongSanPham;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getXuatxu() {
+        return xuatxu;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public int getSoLuongConLai() {
+        return soLuongConLai;
+    }
+
+    public int getTongSanPham() {
+        return tongSanPham;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public void setXuatxu(String xuatxu) {
+        this.xuatxu = xuatxu;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public void setSoLuongConLai(int soLuongConLai) {
+        this.soLuongConLai = soLuongConLai;
+    }
+
+    public void setTongSanPham(int tongSanPham) {
+        this.tongSanPham = tongSanPham;
+    }
+
     
-    public ProductDTO(String proID, String proName, float proPrice, String proOrigin, int proRemain, int proQuantity) {
-        this.proID = proID;
-        this.proName = proName;
-        this.proPrice = proPrice;
-        this.proOrigin = proOrigin;
-        this.proRemain = proRemain;
-        this.proQuantity = proQuantity;
-    }
+
     
-    public ProductDTO(){}
-    
-
-    public String getProID() {
-        return proID;
-    }
-
-    public void setProID(String proID) {
-        this.proID = proID;
-    }
-
-    public String getProName() {
-        return proName;
-    }
-
-    public void setProName(String proName) {
-        this.proName = proName;
-    }
-
-    public float getProPrice() {
-        return proPrice;
-    }
-
-    public void setProPrice(float proPrice) {
-        this.proPrice = proPrice;
-    }
-
-    public String getProOrigin() {
-        return proOrigin;
-    }
-
-    public void setProOrigin(String proOrigin) {
-        this.proOrigin = proOrigin;
-    }
-
-    public int getProRemain() {
-        return proRemain;
-    }
-
-    public void setProRemain(int proRemain) {
-        this.proRemain = proRemain;
-    }
-
-    public int getProQuantity() {
-        return proQuantity;
-    }
-
-    public void setProQuantity(int proQuantity) {
-        this.proQuantity = proQuantity;
-    }
     
     
 }
