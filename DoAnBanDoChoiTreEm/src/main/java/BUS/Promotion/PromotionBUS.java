a48460f888540b917e6e8543b8cc4136dbf59847
zaikaman <91252891+zaikaman@users.noreply.github.com>
2024-05-08 19:08:17 +0700

    Add files via upload

---
+++ DoAnBanDoChoiTreEm/src/main/java/BUS/Promotion/PromotionBUS.java
@@ -0,0 +1,30 @@
+// PromotionBUS.java
+package Bus;
+
+import Inventory.DAO.PromotionDAO;
+import Inventory.DTO.PromotionDTO;
+import java.util.ArrayList;
+
+public class PromotionBUS {
+    private PromotionDAO dao = new PromotionDAO();
+
+    public ArrayList<PromotionDTO> getAllPromotions() {
+        return dao.getAllPromotions();
+    }
+
+    public ArrayList<PromotionDTO> getPromotionsByKeyword(String keyword) {
+        return dao.getPromotionsByKeyword(keyword);
+    }
+
+    public void addPromotion(PromotionDTO promotion) {
+        dao.addPromotion(promotion);
+    }
+
+    public void removePromotion(int id) {
+        dao.removePromotion(id);
+    }
+
+    public void updatePromotion(PromotionDTO promotion) {
+        dao.updatePromotion(promotion);
+    }
+}
