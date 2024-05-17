07fcb5bad17644f3b86a76029d4cacb3d29a92c5
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-05-18 01:10:32 +0700

    Update EmployeeBUS.java

--- DoAnBanDoChoiTreEm/src/main/java/BUS/Employee/EmployeeBUS.java
+++ DoAnBanDoChoiTreEm/src/main/java/BUS/Employee/EmployeeBUS.java
@@ -1,13 +1,34 @@
-package BUS.Employee;
+package BUS;
 
 import Inventory.DAO.StaffDAO;
 import Inventory.DTO.StaffDTO;
 import java.util.ArrayList;
 
 public class EmployeeBUS {
+
     private StaffDAO dao = new StaffDAO();
+    public static ArrayList <StaffDTO> dsnv;
+    public EmployeeBUS(){}
+
+    public void addUser(StaffDTO staff) {
+        dao.addUser(staff);
+    }
+    
+    public void editUser(StaffDTO staff){
+        dao.editUser(staff);
+    }
+    
+    public void getList(){
+        if(dsnv == null){
+            dsnv = new ArrayList<StaffDTO>();
+            dsnv = dao.getAllStaffs();
+        } else {
+            dsnv = dao.getAllStaffs();
+        }
+    }
 
     public ArrayList<StaffDTO> getAllStaffs() {
+        getList();
         return dao.getAllStaffs();
     }
 
@@ -26,9 +47,12 @@ public class EmployeeBUS {
     public void updateStaff(StaffDTO staff) {
         dao.updateStaff(staff);
     }
+
+    public StaffDTO getStaffByUserName(String tk) {
+        return dao.getStaffByUserName(tk);
+    }
     
-    public void getStaffByUserName(String tk){
-        dao.getStaffByUserName(tk);
+    public int[] getInfoStaff(){
+        return dao.getInfoStaff();
     }
 }
-
