0d0c8ac39fa090ae6cd892564e0ebc3c87ea6513
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-05-18 00:29:04 +0700

    Add files via upload

---
+++ DoAnBanDoChoiTreEm/src/main/java/GUI/Statistical/Statistical.java
@@ -0,0 +1,8 @@
+package UI.Statistical;
+import UI.Employee.EmployeePage;
+public class Statistical {
+       private int tongSp, tongLoaiSp, tongHoaDon, tongKhachHang;
+       private double doanhThu;
+       //staTotal;
+       
+}
