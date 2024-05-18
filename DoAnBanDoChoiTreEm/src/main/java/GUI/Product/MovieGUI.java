d36e5a02d89e6a8382e4958461608ca52d45ad76
Nguyễn Đại Quốc <127086245+NguyenDaiQuoc@users.noreply.github.com>
2024-05-18 10:00:13 +0700

    Update MovieGUI.java

--- DoAnBanDoChoiTreEm/src/main/java/GUI/Product/MovieGUI.java
+++ DoAnBanDoChoiTreEm/src/main/java/GUI/Product/MovieGUI.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package DoAn;
+package UI.Product;
 
 import java.awt.Component;
 import java.awt.FlowLayout;
@@ -21,9 +21,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-import DoAn.ProductBUS;
-import DoAn.DoChoiPhuongTien;
-import DoAn.ProductDAO;
+import Bus.ProductBUS;
+import Inventory.DTO.DoChoiTheoPhim;
 import javax.swing.JOptionPane;
 
 /**
